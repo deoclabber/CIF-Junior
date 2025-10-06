7bc8ea494ce0014ca1386e70304216f0f8bdc2c5
deoclabber <danbaoqin@gmail.com>
2025-10-06 19:57:25 -0400

    Update Class 2 - Simple Num Guessing Game.py

--- CIF-Week-1/Class 2 - Simple Num Guessing Game.py
+++ CIF-Week-1/Class 2 - Simple Num Guessing Game.py
@@ -1,12 +1,17 @@
-#
+#Class 2 - Simple Number Guessing Game - By Daniel Qin
+#Beginning
 from random import randint
 
+#Set random number
 random_number = randint(1, 20)
 
+#User guess
 user_guess = int(input("◞୧⎯⎯୨╰ ❝♚Welcome to the number guessing game!♔❞ ╯୧⎯⎯୨◟\n(Beta v0.1) Guess a number between 1 and 20: "))
 
+#Win or lose
 if user_guess == random_number:
     print("𓆩:*¨༺✧꧁𓊈𒆜You Win!𒆜𓊉꧂✧༻¨*:𓆪")
-    
+    print("-*༺꧁⌋⁋╰* ❝Thank you for trying this game!❞ *╯¶⌊꧂༻*-")
 else:
     print("ৡঌৡ ⋆༺𓆩︎You Lose!𓆪༻⋆ ৡৡঌ ৡঌ The correct number was",random_number,"! ৡৡঌৡ")
+    print("Try again! ৡৡঌ ৡঌ")
