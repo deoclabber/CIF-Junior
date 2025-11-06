8c74c600cb6c18fe8cce385809a87acb7cf15657
Daniel Qin <danbaoqin@gmail.com>
2025-11-05 19:05:06 -0500

    Add files via upload

---
+++ CIF-Week-1/Class 4 - Weather.py
@@ -0,0 +1,11 @@
+weather = [
+    ["sunday", [1, -5, "rain and snow 🌧️🌨️"] ],
+    ["monday", [0, -4, "partly cloudy ⛅"] ],
+    ["tuesday", [3, 1, "mostly cloudy 🌥️"] ],
+    ["wednesday", [6, 0, "partly cloudy ⛅"] ],
+    ["thursday", [5, -1, "slightly cloudy 🌤️"] ],      
+    ["friday", [6, 0, "partly cloudy ⛅"] ],
+    ["saturday", [9, 5, "cloudy ☁️"] ],
+]
+
+print(weather[1][1][1])
