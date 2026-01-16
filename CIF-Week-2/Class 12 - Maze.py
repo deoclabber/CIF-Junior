edc98bed08f5cf67a816207bba4618fb43f5843d
Daniel Qin <danbaoqin@gmail.com>
2026-01-16 10:53:35 -0500

    Add files via upload

---
+++ CIF-Week-2/Class 12 - Maze.py
@@ -0,0 +1,243 @@
+from collections.abc import Set
+import pygame
+pygame.init()
+
+#Colors
+green = (70, 180, 80)
+red = (255, 50, 50)
+yellow = (255, 233, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+#Setup
+width = 900
+height = 900
+my_font = pygame.font.SysFont("helvetica", 30)
+text_surface = my_font.render("Escape the maze!", True, (0, 0, 0))
+my_font2 = pygame.font.SysFont("times new roman", 25)
+text_surface2 = my_font2.render("Start", True, (10, 70, 10))
+my_font3 = pygame.font.SysFont("arial", 60)
+text_surface3 = my_font3.render("You Escaped!", True, (0, 0, 0))
+my_font4 = pygame.font.SysFont("arial", 30)
+text_surface4 = my_font4.render("Failed...", True, (255, 75, 75))
+my_font5 = pygame.font.SysFont("times new roman", 25)
+text_surface5 = my_font5.render("Goal", True, (255, 75, 75))
+player_x = 15 #change to 15 later
+player_y = 100#change to 100 later
+player_x_change = 0
+player_y_change = 0
+player_speed = 9
+enemy_y = 60
+enemy_y_change = 7
+enemy_y2 = 600
+enemy_y2_change = 10
+enemy_y3 = 0
+enemy_y3_change = 7
+
+#Set player and enemy speeds
+player_speed = float(input("Enter the speed of the player. (8-15) (10 recommended)"))
+if player_speed < 8:
+    player_speed = 8
+elif player_speed > 15:
+    player_speed = 15
+enemy_y_change = float(input("Enter the speed of the top-left enemies. (5-15) (10 recommended)"))
+if enemy_y_change < 5:
+    enemy_y_change = 5
+elif enemy_y_change > 15:
+    enemy_y_change = 15
+enemy_y2_change = float(input("Enter the speed of the bottom-left enemies. (7-15) (12 recommended)"))
+if enemy_y2_change < 7:
+    enemy_y2_change = 7
+elif enemy_y2_change > 15:
+    enemy_y2_change = 15
+enemy_y3_change = float(input("Enter the speed of the bottom-right enemies. (5-15) (10 recommended) The game window will load after this."))
+if enemy_y3_change < 5:
+    enemy_y3_change = 5
+elif enemy_y3_change > 15:
+    enemy_y3_change = 15
+
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+#Game Loop
+runtime = True
+while runtime:
+    clock.tick(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runtime = False 
+
+    #Player Movement
+    player_x_change = 0
+    player_y_change = 0
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        player_x_change = -player_speed
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        player_x_change = player_speed
+
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        player_y_change = -player_speed
+
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        player_y_change = player_speed
+
+    if player_x < 0:
+        player_x = 0 
+    if player_x > width - 30:
+        player_x = width - 30
+    if player_y < 0:
+        player_y = 0 
+    if player_y > height - 30:
+        player_y = height - 30
+
+    #Wall and goal and enemy setup
+    screen.fill(white)
+    screen.blit(text_surface, (20, 10))
+    screen.blit(text_surface2, (5, 60))
+
+    goal = pygame.Rect(810, 30, 60, 60)
+    pygame.draw.rect(screen, yellow, goal)
+    screen.blit(text_surface5, (815, 45))
+
+    wall1 = pygame.Rect(60, 60, 30, 390)
+    wall2 = pygame.Rect(60, 420, 180, 30)
+    wall3 = pygame.Rect(0, 510, 330, 30)
+    wall4 = pygame.Rect(330, 150, 30, 390)
+    wall5 = pygame.Rect(60, 60, 300, 30)
+    wall6 = pygame.Rect(180, 330, 180, 30)
+    wall7 = pygame.Rect(90, 240, 150, 30)
+    wall8 = pygame.Rect(180, 150, 180, 30)
+    wall9 = pygame.Rect(420, 0, 30, 630)
+    wall10 = pygame.Rect(330, 0, 30, 60)
+    wall11 = pygame.Rect(90, 600, 330, 30)
+    wall12 = pygame.Rect(60, 600, 30, 210)
+    wall13 = pygame.Rect(420, 720, 30, 180)
+    wall_list = [wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13]
+
+    for wall in wall_list:
+        pygame.draw.rect(screen, black, wall)
+
+    player = pygame.Rect(player_x, player_y, 30, 30)
+    player = pygame.Rect(player_x, player_y, 30, 30)
+    pygame.draw.rect(screen, green, player)
+
+    if player.colliderect(goal):
+        screen.blit(text_surface3, (520, 390))
+        pygame.display.update()
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    waiting = False
+                    runtime = False
+                    quit()
+
+        
+    #First set of enemies
+    enemy1 = pygame.Rect(120, enemy_y, 30, 30)
+    enemy2 = pygame.Rect(210, enemy_y, 30, 30)
+    enemy3 = pygame.Rect(300, enemy_y, 30, 30)
+
+    enemy_list = [enemy1, enemy2, enemy3]
+
+    for enemy in enemy_list:
+        pygame.draw.rect(screen, red, enemy)
+    
+    enemy_y += enemy_y_change
+
+    if enemy_y >= 510:
+        enemy_y = 510
+        enemy_y_change = -enemy_y_change
+    elif enemy_y <= 60:
+        enemy_y = 60
+        enemy_y_change = -enemy_y_change
+    
+    #Second set of enemies    
+    enemy4 = pygame.Rect(120, enemy_y2, 30, 30)
+    enemy5 = pygame.Rect(210, enemy_y2, 30, 30)
+    enemy6 = pygame.Rect(300, enemy_y2, 30, 30)
+    enemy7 = pygame.Rect(390, enemy_y2, 30, 30)
+
+    enemy_list2 = [enemy4, enemy5, enemy6, enemy7]
+
+    for enemy in enemy_list2:
+        pygame.draw.rect(screen, red, enemy)
+    
+    enemy_y2 += enemy_y2_change
+
+    if enemy_y2 >= height - 30:
+        enemy_y2 = height - 30
+        enemy_y2_change = -enemy_y2_change
+    elif enemy_y2 <= 600:
+        enemy_y2 = 600
+        enemy_y2_change = -enemy_y2_change
+    
+    #Third set of enemies
+    enemy8 = pygame.Rect(480, enemy_y3, 30, 30)
+    enemy9 = pygame.Rect(570, enemy_y3, 30, 30)
+    enemy10 = pygame.Rect(660, enemy_y3, 30, 30)
+    enemy11 = pygame.Rect(750, enemy_y3, 30, 30)
+    enemy12 = pygame.Rect(840, enemy_y3, 30, 30)
+    enemy13 = pygame.Rect(480 + 45, enemy_y3 + 300, 30, 30)
+    enemy14 = pygame.Rect(570 + 45, enemy_y3 + 300, 30, 30)
+    enemy15 = pygame.Rect(660 + 45, enemy_y3 + 300, 30, 30)
+    enemy16 = pygame.Rect(750 + 45, enemy_y3 + 300, 30, 30)
+    enemy17 = pygame.Rect(840 + 45, enemy_y3 + 300, 30, 30)
+    enemy_list3 = [enemy8, enemy9, enemy10, enemy11, enemy12, enemy13, enemy14, enemy15, enemy16, enemy17]
+
+    for enemy in enemy_list3:
+        pygame.draw.rect(screen, red, enemy)
+    
+    enemy_y3 += enemy_y3_change
+
+    bottom_limit = 300 + 30
+    if enemy_y3 + bottom_limit >= 900:
+        enemy_y3 = 900 - bottom_limit
+        enemy_y3_change = -enemy_y3_change
+    elif enemy_y3 <= 0:
+        enemy_y3 = 0
+        enemy_y3_change = -enemy_y3_change
+
+    #Enemy collision
+    for enemy in enemy_list + enemy_list2 + enemy_list3:
+        if player.colliderect(enemy):
+            screen.blit(text_surface4, (230, 10))
+            player_x = 15
+            player_y = 100
+            pygame.display.update()
+            waiting = True
+            while waiting:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        waiting = False
+                        runtime = False
+                        quit()
+                    if event.type == pygame.KEYDOWN:
+                        waiting = False
+            
+
+    #Wall collision
+    player_x += player_x_change
+    player = pygame.Rect(player_x, player_y, 30, 30)
+
+    for wall in wall_list:
+        if player.colliderect(wall):
+            if player_x_change > 0:
+                player_x = wall.left - 30
+            if player_x_change < 0:
+                player_x = wall.right
+
+    player_y += player_y_change
+    player = pygame.Rect(player_x, player_y, 30, 30)
+
+    for wall in wall_list:
+        if player.colliderect(wall):
+            if player_y_change > 0:
+                player_y = wall.top - 30
+            if player_y_change < 0:
+                player_y = wall.bottom
+                
+    pygame.display.update()
